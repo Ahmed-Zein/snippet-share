93ca14e1e22e134dac26e956907b319287056cd3
Ahmed-Zein <ahmedzein3652@gmail.com>
2023-12-01 12:02:35 +0200

    fix

--- frontend/js/home.js
+++ frontend/js/home.js
@@ -35,24 +35,19 @@ const loadSnippets = async () => {
       console.log(item);
       const row = document.createElement("tr");
 
-      // Create a link (<a>) element
       const titleLink = document.createElement("a");
-      titleLink.href = "http://localhost:3000/api/share/?id=" + item._id; // Replace "#" with the actual link you want
+      titleLink.href = "http://localhost:3000/api/share/?id=" + item._id;
       titleLink.textContent = item.title;
 
-      // Create a table cell (<td>) and append the link to it
       const titleCell = document.createElement("td");
       titleCell.appendChild(titleLink);
 
-      // Append the cell to the row
       row.appendChild(titleCell);
 
-      // Create and append other cells as needed
       const createdAtCell = document.createElement("td");
       createdAtCell.textContent = item.createdAt.split("T")[0];
       row.appendChild(createdAtCell);
 
-      // Append the row to the table body
       tbody.appendChild(row);
     }
   } catch (e) {
